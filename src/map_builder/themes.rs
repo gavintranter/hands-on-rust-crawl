653420208b87b166f6dc250af34c986419600a81
gavin <gt@example.com>
2021-09-08 13:00:56 +0100

    Initial commit

---
+++ src/map_builder/themes.rs
@@ -0,0 +1,40 @@
+use crate::prelude::*;
+use crate::map_builder::MapTheme;
+
+pub struct DungeonTheme {}
+
+impl DungeonTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self{})
+    }
+}
+
+impl MapTheme for DungeonTheme {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
+        match tile_type {
+            TileType::Wall => to_cp437('#'),
+            TileType::Floor => to_cp437('.'),
+            TileType::Exit => to_cp437('>'),
+        }
+    }
+}
+
+pub struct ForestTheme {}
+
+impl ForestTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self{})
+    }
+}
+
+impl MapTheme for ForestTheme {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
+        match tile_type {
+            TileType::Wall => to_cp437('"'),
+            TileType::Floor => to_cp437(';'),
+            TileType::Exit => to_cp437('>'),
+        }
+    }
+}
+
+
